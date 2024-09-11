de85f99488924befcb3ab97b0766cf0178f8c81f
Chanyeong Lim <dlacksdud2@gmail.com>
2024-09-11 13:55:54 +0900

    card style profile page

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,64 @@
 import './App.css';
 
-import { useState } from 'react';
-
-import viteLogo from '/vite.svg';
-
-import reactLogo from './assets/react.svg';
+// import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const profileInfo = {
+    name: '임찬영',
+    englishName: 'Chanyeong Lim',
+    id: 'asp345',
+    role: [
+      "Dept. of Computer Science & Engineering '23",
+      'Wafflestudio 21.5기',
+      'Backend Developer @SNUTT (Apr 2024~)',
+      'New to Frontend Development',
+    ],
+    github: 'https://github.com/asp345/',
+    instagram: 'https://www.instagram.com/ich_yng44/',
+  };
+  const iconLinks = {
+    github: 'https://cdn.svgporn.com/logos/github-icon.svg',
+    instagram: 'https://cdn.svgporn.com/logos/instagram-icon.svg',
+  };
+  const roleList = profileInfo.role.map((role, index) => {
+    return <li key={index}>{role}</li>;
+  });
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="profile-card">
+        <div className="profile-frame">
+          <img
+            src="https://avatars.githubusercontent.com/u/10955734?v=4"
+            className="profile-picture"
+          />
+          <div className="profile-title">
+            <h2>{profileInfo.name}</h2>
+            <p>
+              {profileInfo.englishName}
+              <br />@{profileInfo.id}
+            </p>
+          </div>
+        </div>
+        <div className="profile-details">
+          <div className="profile-icons">
+            <a
+              href={profileInfo.github}
+              rel="norefferer noreferrer"
+              target="_blank"
+            >
+              <img src={iconLinks.github} className="social-icons" />
+            </a>
+            <a
+              href={profileInfo.instagram}
+              rel="norefferer noreferrer"
+              target="_blank"
+            >
+              <img src={iconLinks.instagram} className="social-icons" />
+            </a>
+          </div>
+          <ul>{roleList}</ul>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
